ace596e2bb58f81e7159ccf0636d6a4a4d8e9979
Mateusz Wocka <mawocka@capgemini.com>
2019-08-20 13:09:25 +0200

    Resolve all issues mentioned in comments

--- src/com/capgemini/productionline/configuration/Jenkins.groovy
+++ src/com/capgemini/productionline/configuration/Jenkins.groovy
@@ -468,15 +468,17 @@ if(maven3Install == null) {
     }
   }
   
-    /**
+  /**
     * <p>
-    *  This method add a new configuration for the Ansible Plugin...
+    *  This method add a new configuration for the Ansible Plugin.
+    *  Example usage: addAnsibleInstalator("if [ ! `which ansible` ]\nthen\nsudo apt-get update\nsudo apt-get install
+    *  ansible -y\nelse echo 'Ansible is already installed'\nfi", "ITaaS Ansible", "/usr/bin/")
     * @param @required commands
-    *    Commands should be used to configure the plugin
+    *    Bash commands or script which should be used to configure the plugin
     * @param @required commandLineInstallerName
     *    Name that should be used to identify the new configuration
     * @param @optional home
-    *    Home
+    *    Tool home path
     * @return
     *    Boolean value which reflects wether the installation was successfull or not
   */
@@ -494,18 +496,18 @@ if(maven3Install == null) {
     for (i in desc.getInstallations()) {
     	installations.push(i)
     }
-
+    println("All existing ansible installators have been loaded.");
     try {
-    installers.add(new CommandInstaller("", commands, home))  
-    properties.add(new InstallSourceProperty(installers))
-    def installation = new AnsibleInstallation(commandLineInstallerName, "", properties)
+      installers.add(new CommandInstaller("", commands, home))  
+      properties.add(new InstallSourceProperty(installers))
+      def installation = new AnsibleInstallation(commandLineInstallerName, "", properties)
 
-    installations.push(installation)
-    desc.setInstallations(installations.toArray(new org.jenkinsci.plugins.ansible.AnsibleInstallation[0]))
+      installations.push(installation)
+      desc.setInstallations(installations.toArray(new org.jenkinsci.plugins.ansible.AnsibleInstallation[0]))
 
-    desc.save()
+      desc.save()
     } catch(Exception ex) {
-         println("Installation error  ");
+         println("Error during ansible installtion.");
          return false;
     }
     return true
@@ -568,21 +570,21 @@ if(maven3Install == null) {
   }
   
   /**
-   * <p>
-   *    The method adds a SSH credential to the Jenkins credential store
-   * @param userName
-   *    Username to be added
-   * @param secret
-   *    Secret used inside SSH
-   * @param credentialID
-   *    ID referencing the credential
-   * @param description
-   *    Credential description
-   * @param SSH_KEY
-   *    Provided SSH key
-   */
-
-  def boolean addJenkinsSshCredentials(String description, String credentialID, String secret, String userName, String SSH_KEY) {
+    * <p>
+    *   The method adds a SSH credential to the Jenkins credential store
+    *   Example usage: addJenkinsSshCredentials('Jenkins Node SSH Key', 'IDCredential', '', 'ubuntu', sh(returnStdout: true, script: 'cat ssh_key'))
+    * @param userName
+    *    Username used to connect through the SSH
+    * @param secret
+    *    Secret used inside SSH connection
+    * @param credentialID
+    *    ID referencing the SSH credential
+    * @param description
+    *    Credential description
+    * @param SSH_KEY
+    *    Provided SSH key
+  */
+  public boolean addJenkinsSshCredentials(String description, String credentialID, String secret, String userName, String SSH_KEY) {
     approveSignature("staticMethod com.cloudbees.plugins.credentials.domains.Domain")
 
     def jenkinsMasterKeyParameters = [
@@ -606,7 +608,13 @@ if(maven3Install == null) {
       jenkinsMasterKeyParameters.secret,
       jenkinsMasterKeyParameters.description
     )
-    return store.addCredentials(domain, privateKey)
+    try {
+      store.addCredentials(domain, privateKey)
+    } catch(Exception ex) {
+         println("Error during SSH credential adding.");
+         return false;
+    }
+    return true
   }
 
   /**
@@ -626,27 +634,40 @@ if(maven3Install == null) {
   }
   
   /**
-   * <p>
-   *    The method adds a SSH credential to the Jenkins credential store
-   * @param agentName
-   *    Jenkins agent name
-   * @param agentIP
-   *    External Jenkins agent IP
-   * @param credentialID
-   *    ID referencing the credential
-   * @param agentDescription
-   *    Jenkins agent description
-   * @param agentHome
-   *    Jenkins agent home
-   * @param agentExecutors
-   *    Jenkins agent executors number
-   * @param agentLabels
-   *    Jenkins agent labels
-   * @param sshPort
-   *    Jenkins agent SSH port
-   */
-
-  public boolean addJenkinsNode(String credentialID, String agentName, String agentIP, String agentDescription, String agentHome, String agentExecutors, String agentLabels, int sshPort) {
+    * <p>
+    *    The method adds a SSH credential to the Jenkins credential store
+    * @param agentName
+    *    Jenkins agent name
+    * @param agentIP
+    *    External Jenkins agent IP
+    * @param credentialID
+    *    ID referencing the credential
+    * @param agentDescription
+    *    Jenkins agent description
+    * @param agentHome
+    *    Jenkins agent home
+    * @param agentExecutors
+    *    Jenkins agent executors number
+    * @param agentLabels
+    *    Jenkins agent labels
+    * @param sshPort
+    *    Jenkins agent SSH port
+    * @param jvmOptions
+    *    Options passed to the java vm.
+    * @param javaPath
+    *    Path to the host jdk installation. If null the jdk will be auto detected.
+    * @param prefixStartSlaveCmd
+    *    This will prefix the start agent command. For instance if you want to execute the command with a different shell.
+    * @param suffixStartSlaveCmd
+    *    This will suffix the start agent command.
+    * @param launchTimeoutSeconds
+    *    Launch timeout in seconds
+    * @param maxNumRetries
+    *    The number of times to retry connection if the SSH connection is refused during initial connect
+    * @param retryWaitTime
+    *    The number of seconds to wait between retries
+  */
+  public boolean addJenkinsNode(String credentialID, String agentName, String agentIP, String agentDescription, String agentHome, String agentExecutors, String agentLabels, int sshPort,  String jvmOptions, String javaPath, String prefixStartSlaveCmd, String suffixStartSlaveCmd, Integer launchTimeoutSeconds, Integer maxNumRetries, Integer retryWaitTime) {
 
     SshHostKeyVerificationStrategy hostKeyVerificationStrategy = new NonVerifyingKeyVerificationStrategy()
     DumbSlave dumb = new DumbSlave(agentName,
@@ -655,10 +676,15 @@ if(maven3Install == null) {
             agentExecutors,
             Mode.EXCLUSIVE,
             agentLabels,
-            new SSHLauncher(agentIP, sshPort, credentialID, "", "", "", "", 60, 3, 15, hostKeyVerificationStrategy),
+            new SSHLauncher(agentIP, sshPort, credentialID, jvmOptions, javaPath, prefixStartSlaveCmd, suffixStartSlaveCmd, launchTimeoutSeconds, maxNumRetries, retryWaitTime, hostKeyVerificationStrategy),
             RetentionStrategy.INSTANCE)
-    Jenkins.instance.addNode(dumb)
-   return true;
+    try {
+      Jenkins.instance.addNode(dumb)
+    } catch(Exception ex) {
+         println("Error during Jenkins node creation.");
+         return false;
+    }
+    return true
   }
 
   /**
