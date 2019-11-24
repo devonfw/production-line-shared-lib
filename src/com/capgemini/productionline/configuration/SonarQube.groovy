2442c9ba2185fdccb970fbab0467a1612b419d41
Dario Rodriguez Gonzalez <dario.rodriguez-gonzalez@capgemini.com>
2019-11-24 18:29:50 +0100

    a

--- src/com/capgemini/productionline/configuration/SonarQube.groovy
+++ src/com/capgemini/productionline/configuration/SonarQube.groovy
@@ -7,48 +7,48 @@ class SonarQube implements Serializable {
     def context
 
     /**
-    * Initiate a new 'SonarQube' instance by providing a username and the SonarQube base URL.
-    * <p>
-    * @param context
-    *    The pipeline context (usually the parameter should be set to 'this')
-    * @param username
-    *    A valid SonarQube username.
-    * @param sonarQubeBaseUrl
-    *    The base URL of the SonarQube server.
-    * </p>
-    */
-    SonarQube(context, String username)  {
+     * Initiate a new 'SonarQube' instance by providing a username and the SonarQube base URL.
+     * <p>
+     * @param context
+     *    The pipeline context (usually the parameter should be set to 'this')
+     * @param username
+     *    A valid SonarQube username.
+     * @param sonarQubeBaseUrl
+     *    The base URL of the SonarQube server.
+     * </p>
+     */
+    SonarQube(context, String username) {
         this.context = context
         this.username = username
         this.sonarQubeBaseUrl = ProductionLineGlobals.SONARQUBE_BASE_URL
     }
 
     /**
-    * Initiate a new 'SonarQube' instance by providing a username and the SonarQube base URL.
-    * <p>
-    * @param context
-    *    The pipeline context (usually the parameter should be set to 'this')
-    * @param username
-    *    A valid SonarQube username.
-    * @param sonarQubeBaseUrl
-    *    The base URL of the SonarQube server.
-    * </p>
-    */
-    SonarQube(context, String username, String sonarQubeBaseUrl)  {
+     * Initiate a new 'SonarQube' instance by providing a username and the SonarQube base URL.
+     * <p>
+     * @param context
+     *    The pipeline context (usually the parameter should be set to 'this')
+     * @param username
+     *    A valid SonarQube username.
+     * @param sonarQubeBaseUrl
+     *    The base URL of the SonarQube server.
+     * </p>
+     */
+    SonarQube(context, String username, String sonarQubeBaseUrl) {
         this.context = context
         this.username = username
         this.sonarQubeBaseUrl = sonarQubeBaseUrl
     }
 
     /**
-    * Create a new 'SonarQube' API Token for the user of this instance.
-    * <p>
-    * @param tokenName
-    *    The name of the token to create.
-    * @param username
-    *    A valid SonarQube username.
-    * </p>
-    */
+     * Create a new 'SonarQube' API Token for the user of this instance.
+     * <p>
+     * @param tokenName
+     *    The name of the token to create.
+     * @param username
+     *    A valid SonarQube username.
+     * </p>
+     */
     def getAuthToken(String tokenName) {
         def response = this.context.httpRequest consoleLogResponseBody: true, customHeaders: [[maskValue: true, name: 'X-Forwarded-User', value: username], [maskValue: true, name: 'X-Forwarded-Groups', value: 'admins']], httpMode: 'POST', url: this.sonarQubeBaseUrl + '/api/user_tokens/generate?name=' + tokenName
         def parsedJsonResponse = this.context.readJSON text: response.getContent()
@@ -67,8 +67,13 @@ class SonarQube implements Serializable {
         return response.getContent()
     }
 
-    def addWebhook(String webhookName, String webhookUrl){
-        def response = this.context.httpRequest consoleLogResponseBody: true, customHeaders: [[maskValue: true, name: 'X-Forwarded-User', value: username], [maskValue: true, name: 'X-Forwarded-Groups', value: 'admins']], httpMode: 'POST', url: "${this.sonarQubeBaseUrl}/api/webhooks/create?name=${webhookName}&url=${webhookUrl}"
+    def addWebhook(String webhookName, String webhookUrl) {
+        def response
+        if (this.getSonarVersion() > '7.1') {
+            response = this.context.httpRequest consoleLogResponseBody: true, customHeaders: [[maskValue: true, name: 'X-Forwarded-User', value: username], [maskValue: true, name: 'X-Forwarded-Groups', value: 'admins']], httpMode: 'POST', url: "${this.sonarQubeBaseUrl}/api/webhooks/create?name=${webhookName}&url=${webhookUrl}"
+        } else {
+            response = this.context.httpRequest consoleLogResponseBody: true, customHeaders: [[maskValue: true, name: 'X-Forwarded-User', value: username], [maskValue: true, name: 'X-Forwarded-Groups', value: 'admins']], httpMode: 'POST', url: "${this.sonarQubeBaseUrl}/api/settings/set?key=sonar.webhooks.global&values=jenkins&values=http%3A%2F%2Fjenkins-core%3A8080%2Fjenkins%2Fsonarqube-webhook%2F"
+        }
         return response.getContent()
     }
 
